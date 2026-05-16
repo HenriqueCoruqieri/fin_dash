f37f40a22fbd840a9508103bb3311e66910b2f64
HenriqueCoruqieri <heoliveirac@gmail.com>
2026-05-16 13:42:06 -0300

    feat: add CreateUserController and validation

--- src/repositores/postgres/create-user.js
+++ src/repositores/postgres/create-user.js
@@ -1,8 +1,8 @@
-import { PostgresHelper } from '../../db/postgres/helper'
+import { PostgresHelper } from '../../db/postgres/helper.js'
 
 export class PostgreCreateUserRepository {
     async execute(createUserParams) {
-        const results = await PostgresHelper.query(
+        await PostgresHelper.query(
             'INSERT INTO users(id, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5)',
             [
                 createUserParams.id,
@@ -12,6 +12,12 @@ export class PostgreCreateUserRepository {
                 createUserParams.password,
             ],
         )
-        return results[0]
+
+        const createdUser = await PostgresHelper.query(
+            'SELECT * FROM users WHERE id = $1',
+            [createUserParams.id],
+        )
+
+        return createdUser[0]
     }
 }
